1dfef306df3216ffe348e95f88069da39a792084
Vladislav Proyaev <73171654+vproyaev@users.noreply.github.com>
2021-08-18 01:57:17 +0300

    Delete test.py

--- templates/test.py
+++
@@ -1,7 +0,0 @@
-from headhunter.models import Company, Specialty, Vacancy
-
-companies = Company.objects.all()
-specialty = Specialty.objects.all()
-vac_spec = Vacancy.objects.all()
-
-print(companies)
